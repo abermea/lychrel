1f2a0bcd6f790051084ec768ee580cea10af3015
Adrian Bermea <abermea@outlook.com>
2023-04-17 06:21:54 -0600

    better code formatting

--- main.py
+++ main.py
@@ -7,27 +7,31 @@ def sum_number(num: int):
 def test_palindrome(num: int):
     return str(num) == str(num)[::-1]
 
+def lycrel_test_on_range(start: int, end: int, max_steps: int): 
+    results = []
+
+    for x in range(start, end):
+        num = x
+        steps = 0
+
+        while test_palindrome(num) is not True and steps < max_steps:
+            steps += 1
+            num = sum_number(num)
+        
+        result = {
+            "number" : x,
+            "isPalindrome": True if steps < max_steps else False,
+            "steps" : steps
+        }
+
+        results.append(result) 
+
+    return results
+
 start = 0 # Where to start
 end = 2000 # Where to end
 max_steps = 500 # Max attempts before making a failure to prevent infinite loops
 
-# Arrays to hold numbers 
-results = []
-
-for x in range(start, end):
-    num = x
-    steps = 0
-
-    while test_palindrome(num) is not True and steps < max_steps:
-        steps += 1
-        num = sum_number(num)
-    
-    result = {
-        "number" : x,
-        "isPalindrome": True if steps < max_steps else False,
-        "steps" : steps
-    }
-
-    results.append(result)    
+results = lycrel_test_on_range(start, end, max_steps)
 
 print("Failed: {}".format(list(filter(lambda x : x["isPalindrome"] == False, results))))
